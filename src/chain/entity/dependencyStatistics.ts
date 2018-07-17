639ff3df2aa28c2f75a8abe30a10b46172a6a815
bnjm <benjamin.krenn@leftshift.one>
2018-07-17 14:09:39 +0200

    added outdatedPercentage

--- src/chain/entity/dependencyStatistics.ts
+++ src/chain/entity/dependencyStatistics.ts
@@ -5,9 +5,18 @@
 export class DependencyStatistics {
     readonly totalDependencies: number;
     readonly outdatedDependencies: number;
+    readonly outdatedPercentage: number;
 
     constructor(totalDependencies: number, outdatedDependencies: number) {
         this.totalDependencies = totalDependencies;
         this.outdatedDependencies = outdatedDependencies;
+        this.outdatedPercentage = this.calculatePercentage(totalDependencies, outdatedDependencies);
+    }
+
+    private calculatePercentage(totalDependencies: number, outdatedDependencies: number) : number {
+        if (totalDependencies === 0) {
+            return 0;
+        }
+        return Math.round(outdatedDependencies / totalDependencies * 100);
     }
 }
